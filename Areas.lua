73bf261131652e5aad8f890d0a34cbb67fd23479
deathstrats <83128987+deathstrats@users.noreply.github.com>
2025-09-10 19:15:00 +0700

    Add files via upload

---
+++ Areas.lua
@@ -0,0 +1,290 @@
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:20
+-- Luau version 6, Types version 3
+-- Time taken: 0.001157 seconds
+-- Path: ReplicatedStorage/Areas
+
+return {
+    ["Coral Reefs"] = {
+        BaseLuck = 0;
+        ClickPowerMultiplier = 0.8;
+        Cover = "rbxassetid://71296896753311";
+        Items = {
+            [1] = "Salmon";
+            [10] = "Maze Angelfish";
+            [11] = "Blue Lobster";
+            [12] = "Hawks Turtle";
+            [13] = "Starjam Tang";
+            [14] = "Greenbee Grouper";
+            [15] = "Enchant Stone";
+            [16] = "Sail Tang";
+            [17] = "Flat Fish";
+            [18] = "Flying Fish";
+            [19] = "Lion Fish";
+            [2] = "Corazon Damsel";
+            [20] = "Luminous Fish";
+            [21] = "Pink Dolphin";
+            [22] = "Saw Fish";
+            [23] = "Starfish";
+            [24] = "Wahoo";
+            [25] = "Eerie Shark";
+            [26] = "Monster Shark";
+            [3] = "Orangy Goby";
+            [4] = "Specked Butterfly";
+            [5] = "Tricolore Butterfly";
+            [6] = "White Clownfish";
+            [7] = "Panther Grouper";
+            [8] = "Scissortail Dartfish";
+            [9] = "Domino Damsel";
+        };
+        Order = 5;
+    };
+    ["Crater Island"] = {
+        BaseLuck = 0;
+        ClickPowerMultiplier = 0.35;
+        Cover = "rbxassetid://91202432476960";
+        Items = {
+            [1] = "Frostborn Shark";
+            [10] = "Catfish";
+            [11] = "Sheepshead Fish";
+            [12] = "Coney Fish";
+            [13] = "Blackcap Basslet";
+            [2] = "Plasma Shark";
+            [3] = "Axolotl";
+            [4] = "Silver Tuna";
+            [5] = "Pilot Fish";
+            [6] = "Patriot Tang";
+            [7] = "Red Snapper";
+            [8] = "Parrot Fish";
+            [9] = "Hermit Crab";
+        };
+        Order = 8;
+    };
+    ["Esoteric Depths"] = {
+        BaseLuck = 0;
+        ClickPowerMultiplier = 0.55;
+        Cover = "rbxassetid://70827963357705";
+        Items = {
+            [1] = "Rockfish";
+            [10] = "Enchant Stone";
+            [11] = "Thin Armor Shark";
+            [12] = "Scare";
+            [2] = "Coal Tang";
+            [3] = "Ash Basslet";
+            [4] = "Charmed Tang";
+            [5] = "Dark Tentacle";
+            [6] = "Astra Damsel";
+            [7] = "Enchanted Angelfish";
+            [8] = "Magic Tang";
+            [9] = "Abyss Seahorse";
+        };
+        Order = 6;
+    };
+    ["Fisherman Island"] = {
+        BaseLuck = 0;
+        ClickPowerMultiplier = 1;
+        Cover = "rbxassetid://91009938122602";
+        Items = {
+            [1] = "Azure Damsel";
+            [10] = "Clownfish";
+            [11] = "Dorhey Tang";
+            [12] = "Unicorn Tang";
+            [13] = "Dotted Stingray";
+            [14] = "Enchant Stone";
+            [15] = "Bandit Angelfish";
+            [16] = "Lined Cardinal Fish";
+            [17] = "Pygmy Goby";
+            [18] = "Watanabei Angelfish";
+            [19] = "White Tang";
+            [2] = "Flame Angelfish";
+            [20] = "Herring Fish";
+            [21] = "Barracuda Fish";
+            [22] = "Gar Fish";
+            [23] = "Frog";
+            [24] = "Lake Sturgeon";
+            [25] = "Crystal Crab";
+            [26] = "Orca";
+            [3] = "Korean Angelfish";
+            [4] = "Ballina Angelfish";
+            [5] = "Yello Damselfish";
+            [6] = "Strawberry Dotty";
+            [7] = "Yellowfin Tuna";
+            [8] = "Copperband Butterfly";
+            [9] = "Darwin Clownfish";
+        };
+        Order = -10;
+    };
+    Kohana = {
+        BaseLuck = 0;
+        ClickPowerMultiplier = 1;
+        Cover = "rbxassetid://136679349504933";
+        Items = {
+            [1] = "Fire Goby";
+            [10] = "Lobster";
+            [11] = "Loggerhead Turtle";
+            [12] = "Bumblebee Grouper";
+            [13] = "Boa Angelfish";
+            [14] = "Prismy Seahorse";
+            [15] = "Enchant Stone";
+            [16] = "Zoster Butterfly";
+            [17] = "Blumato Clownfish";
+            [2] = "Reef Chromis";
+            [3] = "Shrimp Goby";
+            [4] = "Banded Butterfly";
+            [5] = "Queen Angelfish";
+            [6] = "Jennifer Dottyback";
+            [7] = "Longnose Butterfly";
+            [8] = "Sushi Cardinal";
+            [9] = "Kau Cardinal";
+        };
+        Order = 3;
+    };
+    ["Kohana Volcano"] = {
+        BaseLuck = 0;
+        ClickPowerMultiplier = 0.8;
+        Cover = "rbxassetid://81420844408191";
+        Items = {
+            [1] = "Magma Goby";
+            [2] = "Firecoal Damsel";
+            [3] = "Volcanic Basslet";
+            [4] = "Lavafin Tuna";
+            [5] = "Blueflame Ray";
+            [6] = "Lava Butterfly";
+            [7] = "Rockform Cardianl";
+            [8] = "Volsail Tang";
+        };
+        Order = 4;
+    };
+    ["Lost Isle"] = {
+        BaseLuck = 0;
+        ClickPowerMultiplier = 0.15;
+        Cover = "rbxassetid://134411025211886";
+        Hidden = true;
+        Items = {
+            [1] = "Dead Fish";
+            [2] = "Skeleton Fish";
+            [3] = "Swordfish";
+        };
+        Order = 10;
+    };
+    Ocean = {
+        BaseLuck = 0;
+        ClickPowerMultiplier = 1;
+        Cover = "rbxassetid://86832426448448";
+        Items = {
+            [1] = "Candy Butterfly";
+            [10] = "Slurpfish Chromis";
+            [11] = "Manta Ray";
+            [12] = "Chrome Tuna";
+            [13] = "Hammerhead Shark";
+            [14] = "Enchant Stone";
+            [15] = "Blob Shark";
+            [16] = "Ghost Shark";
+            [17] = "Masked Angelfish";
+            [18] = "Blue-Banded Goby";
+            [19] = "Conspi Angelfish";
+            [2] = "Jewel Tang";
+            [20] = "Fade Tang";
+            [21] = "Worm Fish";
+            [3] = "Skunk Tilefish";
+            [4] = "Vintage Damsel";
+            [5] = "Yellowstate Angelfish";
+            [6] = "Vintage Blue Tang";
+            [7] = "Cow Clownfish";
+            [8] = "Moorish Idol";
+            [9] = "Tricolore Butterfly";
+        };
+        Order = -5;
+    };
+    ["Sisyphus Statue"] = {
+        BaseLuck = 0;
+        ClickPowerMultiplier = 0.15;
+        Cover = "rbxassetid://117010035055721";
+        Items = {
+            [1] = "Electric Eel";
+            [10] = "Fang Tooth Fish";
+            [11] = "Armor Catfish";
+            [12] = "Blob Fish";
+            [13] = "Angler Fish";
+            [14] = "Deep Sea Crab";
+            [15] = "Giant Squid";
+            [16] = "Robot Kraken";
+            [2] = "Boar Fish";
+            [3] = "Dark Eel";
+            [4] = "Viperfish";
+            [5] = "Vampire Squid";
+            [6] = "Spotted Lantern Fish";
+            [7] = "Monk Fish";
+            [8] = "Jellyfish";
+            [9] = "Synodontis";
+        };
+        Order = 11;
+    };
+    ["Sparkling Cove"] = {
+        BaseLuck = 1;
+        ClickPowerMultiplier = 1;
+        Cover = "rbxassetid://70827963357705";
+        Hidden = true;
+        Items = {
+            [1] = "Azure Damsel";
+            [10] = "Clownfish";
+            [11] = "Dorhey Tang";
+            [12] = "Unicorn Tang";
+            [13] = "Dotted Stingray";
+            [14] = "Enchant Stone";
+            [2] = "Flame Angelfish";
+            [3] = "Korean Angelfish";
+            [4] = "Ballina Angelfish";
+            [5] = "Yello Damselfish";
+            [6] = "Strawberry Dotty";
+            [7] = "Yellowfin Tuna";
+            [8] = "Copperband Butterfly";
+            [9] = "Darwin Clownfish";
+        };
+        Order = 100;
+    };
+    ["Treasure Room"] = {
+        BaseLuck = 0;
+        ClickPowerMultiplier = 0.15;
+        Cover = "rbxassetid://79238405720201";
+        Items = {
+            [1] = "Dead Fish";
+            [10] = "Giant Squid";
+            [11] = "Fang Tooth Fish";
+            [12] = "Armor Catfish";
+            [13] = "Blob Fish";
+            [14] = "Angler Fish";
+            [15] = "Deep Sea Crab";
+            [16] = "King Crab";
+            [17] = "Queen Crab";
+            [2] = "Skeleton Fish";
+            [3] = "Swordfish";
+            [4] = "Viperfish";
+            [5] = "Vampire Squid";
+            [6] = "Spotted Lantern Fish";
+            [7] = "Monk Fish";
+            [8] = "Jellyfish";
+            [9] = "Synodontis";
+        };
+        Order = 11;
+    };
+    ["Tropical Grove"] = {
+        BaseLuck = 0;
+        ClickPowerMultiplier = 0.45;
+        Cover = "rbxassetid://77908303381120";
+        Items = {
+            [1] = "Strippled Seahorse";
+            [2] = "Orange Basslet";
+            [3] = "Pufferfish";
+            [4] = "Racoon Butterfly Fish";
+            [5] = "Scissortail Dartfish";
+            [6] = "Thresher Shark";
+            [7] = "Great Whale";
+            [8] = "Enchant Stone";
+        };
+        Order = 7;
+    };
+,
+	_moduleScript = script;
+}
