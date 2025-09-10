1ab37ee274b0fe7854c648ba693b3453dd3ff5b8
deathstrats <83128987+deathstrats@users.noreply.github.com>
2025-09-10 19:22:04 +0700

    Add files via upload

---
+++ mutasi.lua
@@ -0,0 +1,505 @@
+
+
+
+--- Albino.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:21
+-- Luau version 6, Types version 3
+-- Time taken: 0.000100 seconds
+-- Path: ReplicatedStorage/Variants/Albino
+
+return {
+    Colors = 2;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 14;
+        Name = "Albino";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 2.5;
+    };
+    SellMultiplier = 1.3;
+    Versions = 1;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
+
+
+--- Corrupt.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:20
+-- Luau version 6, Types version 3
+-- Time taken: 0.000086 seconds
+-- Path: ReplicatedStorage/Variants/Corrupt
+
+return {
+    Colors = 5;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 1;
+        Name = "Corrupt";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 0.5;
+    };
+    SellMultiplier = 3;
+    Versions = 1;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
+
+
+--- Fairy_Dust.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:20
+-- Luau version 6, Types version 3
+-- Time taken: 0.000098 seconds
+-- Path: ReplicatedStorage/Variants/Fairy Dust
+
+return {
+    Colors = 5;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 6;
+        Name = "Fairy Dust";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 0.6;
+    };
+    SellMultiplier = 2.8;
+    Versions = 1;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
+
+
+--- Festive.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:20
+-- Luau version 6, Types version 3
+-- Time taken: 0.000090 seconds
+-- Path: ReplicatedStorage/Variants/Festive
+
+return {
+    Colors = 5;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 11;
+        Name = "Festive";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 2.2;
+    };
+    SellMultiplier = 2.6;
+    Versions = 1;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
+
+
+--- Frozen.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:20
+-- Luau version 6, Types version 3
+-- Time taken: 0.000096 seconds
+-- Path: ReplicatedStorage/Variants/Frozen
+
+return {
+    Colors = 3;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 12;
+        Name = "Frozen";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 5.5;
+    };
+    SellMultiplier = 2;
+    Versions = 2;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
+
+
+--- Galaxy.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:20
+-- Luau version 6, Types version 3
+-- Time taken: 0.000096 seconds
+-- Path: ReplicatedStorage/Variants/Galaxy
+
+return {
+    Colors = 4;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 2;
+        Name = "Galaxy";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 0.5;
+    };
+    SellMultiplier = 5.5;
+    Versions = 1;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
+
+
+--- Gemstone.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:20
+-- Luau version 6, Types version 3
+-- Time taken: 0.000086 seconds
+-- Path: ReplicatedStorage/Variants/Gemstone
+
+return {
+    Colors = 4;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 3;
+        Name = "Gemstone";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 0.6;
+    };
+    SellMultiplier = 3.8;
+    Versions = 3;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
+
+
+--- Ghost.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:20
+-- Luau version 6, Types version 3
+-- Time taken: 0.000211 seconds
+-- Path: ReplicatedStorage/Variants/Ghost
+
+return {
+    Colors = 5;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 4;
+        Name = "Ghost";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 1.4;
+    };
+    SellMultiplier = 2.5;
+    Versions = 1;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
+
+
+--- Gold.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:20
+-- Luau version 6, Types version 3
+-- Time taken: 0.000217 seconds
+-- Path: ReplicatedStorage/Variants/Gold
+
+return {
+    Colors = 5;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 7;
+        Name = "Gold";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 1.6;
+    };
+    SellMultiplier = 2.8;
+    Versions = 1;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
+
+
+--- Holographic.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:21
+-- Luau version 6, Types version 3
+-- Time taken: 0.000088 seconds
+-- Path: ReplicatedStorage/Variants/Holographic
+
+return {
+    Colors = 5;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 13;
+        Name = "Holographic";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 0.9;
+    };
+    SellMultiplier = 2.4;
+    Versions = 1;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
+
+
+--- Lightning.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:20
+-- Luau version 6, Types version 3
+-- Time taken: 0.000094 seconds
+-- Path: ReplicatedStorage/Variants/Lightning
+
+return {
+    Colors = 5;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 5;
+        Name = "Lightning";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 0.9;
+    };
+    SellMultiplier = 3.2;
+    Versions = 1;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
+
+
+--- Midnight.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:21
+-- Luau version 6, Types version 3
+-- Time taken: 0.000331 seconds
+-- Path: ReplicatedStorage/Variants/Midnight
+
+return {
+    Colors = 4;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 8;
+        Name = "Midnight";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 0.7;
+    };
+    SellMultiplier = 3.8;
+    Versions = 1;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
+
+
+--- Radioactive.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:21
+-- Luau version 6, Types version 3
+-- Time taken: 0.000089 seconds
+-- Path: ReplicatedStorage/Variants/Radioactive
+
+return {
+    Colors = 4;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 9;
+        Name = "Radioactive";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 0.9;
+    };
+    SellMultiplier = 3;
+    Versions = 1;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
+
+
+--- Stone.lua ---
+
+
+
+-- Decompiler will be improved VERY SOON!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/brNTY8nX8t)
+-- Decompiled on 2025-09-10 17:08:21
+-- Luau version 6, Types version 3
+-- Time taken: 0.000083 seconds
+-- Path: ReplicatedStorage/Variants/Stone
+
+return {
+    Colors = 4;
+    Data = {
+        Description = "";
+        Icon = "";
+        Id = 10;
+        Name = "Stone";
+        TierColor = nil;
+        Type = "Variant";
+    };
+    Modifiers = {
+    };
+    Probability = {
+        Chance = 4;
+    };
+    SellMultiplier = 1.2;
+    Versions = 1;
+    _moduleScript = nil;
+,
+	_moduleScript = script;
+}
+
+
